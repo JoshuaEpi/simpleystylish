c7ff40b36843c6c514078c651a1c37b9342d2d44
Joshua <joshuaepifanio18@outlook.com>
2024-05-18 13:01:00 -0500

    Initial commit

---
+++ src/java/Modelo/EmpleadoDAO.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class EmpleadoDAO {
+
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+
+    public Empleado validar(String user, String dni) {
+        Empleado em = new Empleado();
+        String sql = "Select * from empleado where user=? and Dni=?";
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, user);
+            ps.setString(2, dni);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                em.setId(rs.getInt("Empleado"));
+                em.setUser(rs.getString("user"));
+                em.setDni(rs.getString("Dni"));
+                em.setNom(rs.getString("Nombres"));
+            }
+        } catch (Exception e) {
+        }
+        return em;
+    }
+}
