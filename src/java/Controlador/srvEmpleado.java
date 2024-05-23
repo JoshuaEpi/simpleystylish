063179e2aac7fb108c4b70a1d08a27ac993a043e
OliverTx <oliver4185@gmail.com>
2024-05-23 02:49:33 -0500

    modificarse

--- src/java/Controlador/srvEmpleado.java
+++ src/java/Controlador/srvEmpleado.java
@@ -7,32 +7,63 @@ package Controlador;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author OLIVER
- */
+@WebServlet(name = "srvEmpleado", urlPatterns = {"/srvEmpleado"})
 public class srvEmpleado extends HttpServlet {
 
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet srvEmpleado</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet srvEmpleado at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String accion = request.getParameter("accion");
+        try {
+            if (accion != null) {
+                switch (accion) {
+                    case "verificar":
+                        
+                        break;
+                    case "cerrar":
+                        
+                    case "listarEmpleado":
+                        
+                        break;
+                    case "nuevo":
+                        
+                        break;
+                    case "registrar":
+                        
+                        break;
+                    case "leerEmpleado":
+                        
+                        break;
+                    case "actualizarEmpleado":
+                        
+                        break;
+                    case "eliminarEmpleado":
+                        
+                        break;
+                    default:
+                        response.sendRedirect("identificar.jsp");
+                }
+            } else if (request.getParameter("cambiar") != null) {
+                cambiarEstado(request, response);
+            } else {
+                response.sendRedirect("identificar.jsp");
+            }
+        } catch (Exception e) {
+            try {
+                this.getServletConfig().getServletContext().getRequestDispatcher("/mensaje.jsp").forward(request, response);
+
+            } catch (Exception ex) {
+                System.out.println("Error" + e.getMessage());
+            }
         }
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -73,5 +104,9 @@ public class srvEmpleado extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private void cambiarEstado(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
