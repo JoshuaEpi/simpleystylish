1b4d2d9fada5770c3c78bc61b23e484f148ab4ca
OliverTx <oliver4185@gmail.com>
2024-05-23 01:59:53 -0500

    mod

--- src/java/Modelo/CategoriaDAO.java
+++ src/java/Modelo/CategoriaDAO.java
@@ -1,10 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CategoriaDAO {
-    
+
+public class CategoriaDAO extends Conexion {
+    public List<categoria> listarCategoria() throws Exception {
+        List<categoria> categoria;
+        categoria cat;
+        ResultSet rs = null;
+        String sql = "SELECT C.IDCATEGORIA, C.NOMBRECATEGORIA, C.ESTADO FROM CATEGORIA C "
+                + "ORDER BY C.IDCATEGORIA";
+
+        try {
+            this.conectar(false);
+            rs = this.ejecutarOrdenDatos(sql);
+            categoria = new ArrayList<>();
+            while (rs.next() == true) {
+                cat = new categoria();
+                cat.setId_categoria(rs.getInt("IDCATEGORIA"));
+                cat.setNombreCategoria(rs.getString("NOMBRECATEGORIA"));
+                cat.setEstado(rs.getBoolean("ESTADO"));
+                categoria.add(cat);
+            }
+            this.cerrar(true);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+        }
+        return categoria;
+    }
 }
+
+
--- src/java/Modelo/MarcaDAO.java
+++ src/java/Modelo/MarcaDAO.java
@@ -1,7 +1,36 @@
 
 package Modelo;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MarcaDAO {
+
+public class MarcaDAO extends Conexion{
     
+    public List<marca> listarMarca() throws Exception {
+        List<marca> marca;
+        marca mar;
+        ResultSet rs = null;
+        String sql = "SELECT M.IDMARCA, M.NOMBREMARCA, M.ESTADO FROM MARCA M "
+                + "ORDER BY M.IDMARCA";
+
+        try {
+            this.conectar(false);
+            rs = this.ejecutarOrdenDatos(sql);
+            marca = new ArrayList<>();
+            while (rs.next() == true) {
+                mar = new marca();
+                mar.setId_marca(rs.getInt("IDMARCA"));
+                mar.setNombreMarca(rs.getString("NOMBREMARCA"));
+                mar.setEstado(rs.getBoolean("ESTADO"));
+                marca.add(mar);
+            }
+            this.cerrar(true);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+        }
+        return marca;
+    }
 }
