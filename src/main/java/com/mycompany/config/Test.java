372d071fe1617ad03271631cdfe1894ad5d80605
OliverTx <oliver4185@gmail.com>
2024-06-18 02:50:25 -0500

    Renovado

---
+++ src/main/java/com/mycompany/config/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.config;
+
+/**
+ *
+ * @author OLIVER
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        Conexion.getConnection();
+    }
+    
+}
